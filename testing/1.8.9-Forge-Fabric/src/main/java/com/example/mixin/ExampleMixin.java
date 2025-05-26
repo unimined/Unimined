38c368757f4b6c30c71e1888ab93a746f3df9b1b
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-05-25 22:02:24 -0500

    use feather for forge 1.8.9 test

--- testing/1.8.9-Forge-Fabric/src/main/java/com/example/mixin/ExampleMixin.java
+++ testing/1.8.9-Forge-Fabric/src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,16 +1,16 @@
 package com.example.mixin;
 
 import com.example.ExampleMod;
-import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiMainMenu.class)
+@Mixin(TitleScreen.class)
 public class ExampleMixin {
 
-    @Inject(method = "initGui()V", at = @At("HEAD"))
+    @Inject(method = "init()V", at = @At("HEAD"))
     private void init(CallbackInfo info) {
         ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
     }
