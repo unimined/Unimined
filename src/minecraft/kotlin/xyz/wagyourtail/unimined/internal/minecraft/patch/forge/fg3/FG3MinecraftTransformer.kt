150b3a507089f75e156977b5943da067473d9432
joe <burtonjae@hotmail.co.uk>
2025-07-20 12:00:00 +0100

    Generate Minecraft-Dists in neoforge client-extra manifest

--- src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/forge/fg3/FG3MinecraftTransformer.kt
+++ src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/forge/fg3/FG3MinecraftTransformer.kt
@@ -9,12 +9,10 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ExternalDependency
-import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.LogLevel
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.annotations.VisibleForTesting
-import xyz.wagyourtail.unimined.*
 import xyz.wagyourtail.unimined.api.minecraft.EnvType
 import xyz.wagyourtail.unimined.api.minecraft.MinecraftJar
 import xyz.wagyourtail.unimined.api.minecraft.task.AbstractRemapJarTask
@@ -281,7 +279,7 @@ open class FG3MinecraftTransformer(project: Project, val parent: ForgeLikeMinecr
 
     private fun createClientExtra(
         baseMinecraftClient: MinecraftJar,
-        @Suppress("UNUSED_PARAMETER") baseMinecraftServer: MinecraftJar?,
+        baseMinecraftServer: MinecraftJar?,
         patchedMinecraft: Path
     ) {
         val clientExtra = patchedMinecraft.parent.createDirectories()
@@ -317,7 +315,12 @@ open class FG3MinecraftTransformer(project: Project, val parent: ForgeLikeMinecr
                     }
                 }
             }
+
+            writeClientExtraManifest(extra.getPath("META-INF/MANIFEST.MF"), baseMinecraftClient, baseMinecraftServer)
         }
+    }
+
+    protected open fun writeClientExtraManifest(manifestFile: Path, baseMinecraftClient: MinecraftJar, baseMinecraftServer: MinecraftJar?) {
     }
 
     override fun transform(minecraft: MinecraftJar): MinecraftJar {
