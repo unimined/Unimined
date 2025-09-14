f9001144753a58ce9fa697b4cdf0af2a89cb3d4c
xEricL <37921711+xEricL@users.noreply.github.com>
2025-09-14 02:48:54 -0400

    fix: 1.8.9-Forge-Fabric test
    - VideoSettingsMixin was broken after switching to feather mappings in 38c368757f4b6c30c71e1888ab93a746f3df9b1b

--- testing/1.8.9-Forge-Fabric/src/fabric/java/com/example/fabric/mixin/VideoSettingsMixin.java
+++ testing/1.8.9-Forge-Fabric/src/fabric/java/com/example/fabric/mixin/VideoSettingsMixin.java
@@ -2,30 +2,30 @@ package com.example.fabric.mixin;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiVideoSettings;
+import net.minecraft.client.gui.screen.VideoOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiVideoSettings.class)
+@Mixin(VideoOptionsScreen.class)
 public class VideoSettingsMixin {
 
-    @Definition(id = "id", field = "Lnet/minecraft/client/gui/GuiButton;id:I")
+    @Definition(id = "id", field = "Lnet/minecraft/client/gui/widget/ButtonWidget;id:I")
     @Expression("?.id == 200")
-    @Inject(method = "actionPerformed", at = @At("MIXINEXTRAS:EXPRESSION"))
+    @Inject(method = "buttonClicked", at = @At("MIXINEXTRAS:EXPRESSION"))
     private void updateId(
-            GuiButton button,
+            ButtonWidget button,
             CallbackInfo info
     ) {
         System.out.println("button pressed!");
     }
 
-    @Definition(id = "saveOptions", method = "Lnet/minecraft/client/settings/GameSettings;saveOptions()V")
-    @Expression("?.saveOptions()")
-    @Inject(method = "actionPerformed", at = @At("MIXINEXTRAS:EXPRESSION"))
-    private void onSave(GuiButton button, CallbackInfo ci) {
+    @Definition(id = "save", method = "Lnet/minecraft/client/options/GameOptions;save()V")
+    @Expression("?.save()")
+    @Inject(method = "buttonClicked", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private void onSave(ButtonWidget button, CallbackInfo ci) {
         System.out.println("saving options!");
     }
 
