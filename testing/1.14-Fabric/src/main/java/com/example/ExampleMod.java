f891f129567963fa2181a5a31004e8e1e7881f0a
xEricL <37921711+xEricL@users.noreply.github.com>
2025-09-14 02:09:46 -0400

    fix: 1.14-Fabric test

--- testing/1.14-Fabric/src/main/java/com/example/ExampleMod.java
+++ testing/1.14-Fabric/src/main/java/com/example/ExampleMod.java
@@ -1,9 +1,10 @@
 package com.example;
 
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod {
+public class ExampleMod implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("modid");
 
 
