32948263891f3e5f57fbcb3c4c42437c59fd6bf2
HypherionSA <hypherionmc@gmail.com>
2025-12-19 22:23:20 +0200

    We should be using Neo's binpatcher on 1.21.9+, not just 1.21.11

--- src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/forge/fg3/FG3MinecraftTransformer.kt
+++ src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/forge/fg3/FG3MinecraftTransformer.kt
@@ -391,7 +391,7 @@ open class FG3MinecraftTransformer(project: Project, val parent: ForgeLikeMinecr
         if (!patchedMC.path.exists() || project.unimined.forceReload) {
             patchedMC.path.deleteIfExists()
             val additionalArgs = listOf("--data", "--unpatched")
-            val isModernNeo = (provider.minecraftData.mcVersionCompare("1.21.11", provider.version) == -1 && parent.providerName.equals("NeoForged", true))
+            val isModernNeo = (provider.minecraftData.mcVersionCompare("1.21.9", provider.version) == -1 && parent.providerName.equals("NeoForged", true))
 
             val args = (userdevCfg["binpatcher"].asJsonObject["args"].asJsonArray.map {
                 when (it.asString) {
