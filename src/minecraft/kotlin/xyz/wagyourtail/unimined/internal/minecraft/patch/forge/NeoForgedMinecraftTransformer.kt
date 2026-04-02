0a9c3aeab55bae7af4f7ac6f9ddbb313f64acb2c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-04-02 00:14:20 -0700

    neo 26.1.1 fix

--- src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/forge/NeoForgedMinecraftTransformer.kt
+++ src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/forge/NeoForgedMinecraftTransformer.kt
@@ -41,12 +41,12 @@ open class NeoForgedMinecraftTransformer(project: Project, provider: MinecraftPr
             if (provider.version == "1.20.1") {
                 project.dependencies.create("net.neoforged:forge:${provider.version}-$dep:universal")
             } else {
-                var version = provider.version.removePrefix("1.")
-
-                if (provider.minecraftData.mcVersionCompare(provider.version, "26.1") >= 0) {
-                    version = "${provider.version}.0"
-                } else if (!version.contains(".")) {
-                    version = "$version.0"
+                var version = provider.version
+                if (version.matches(Regex("\\d+\\.\\d+"))) {
+                    version = "${version}.0"
+                }
+                if (provider.minecraftData.mcVersionCompare(version, "26.1") < 0) {
+                    version = version.removePrefix("1.")
                 }
                 project.dependencies.create("net.neoforged:neoforge:$version.$dep:universal")
             }
