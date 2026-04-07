8d8ad4fbb8d7587c0ae9db8b580eac0133896542
Caroline Joy Bell <caroline@halotroop.com>
2026-04-07 04:10:40 -0700

    OmniArchive manifest helper function
    and reset function, in case you need it

--- src/api/kotlin/xyz/wagyourtail/unimined/api/minecraft/resolver/MinecraftData.kt
+++ src/api/kotlin/xyz/wagyourtail/unimined/api/minecraft/resolver/MinecraftData.kt
@@ -10,9 +10,22 @@ abstract class MinecraftData {
     @get:ApiStatus.Internal
     abstract val mcVersionFolder: Path
 
+    /**
+     * The location of the JSON file that lists available versions.
+     *
+     * **Resetting this may cause caching errors.**
+     *
+     * @sample xyz.wagyourtail.unimined.api.minecraft.MinecraftConfig.MOJANG_MANIFEST_V2
+     * @sample xyz.wagyourtail.unimined.api.minecraft.MinecraftConfig.OMNIARCHIVE_MANIFEST
+     */
     @set:ApiStatus.Experimental
     abstract var launcherMetaUrl: URI?
 
+    /**
+     * The location of the JSON file for the target version.
+     *
+     * **Resetting this may cause caching errors.**
+     */
     @set:ApiStatus.Experimental
     abstract var metadataURL: URI
 
